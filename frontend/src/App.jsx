792d6878ef301713a3ce8214f8f27bad47eed526
aryan-ya <ay3978943@gmail.com>
2024-11-11 12:03:45 +0530

    home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import Home from './home/Home'
-
+import Course from './components/Course'
+import {Route , Routes} from "react-router-dom";
 
 function App() {
   return (
     <>
-    <Home />
+    {/* <Home />
+    <Course /> */}
+
+
+    <Routes>
+      <Route path="/" element = {<Home />}></Route>
+      <Route path="/course" element = {<Course />}></Route>
+    </Routes>
     </>
   )
 }

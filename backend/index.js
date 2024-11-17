7d426d65a2f6c5668ecd6b0671c2ad0db6d45c1c
aryan-ya <ay3978943@gmail.com>
2024-11-17 20:42:21 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,12 @@ import dotenv from "dotenv";
 import cors from "cors";
 import mongoose from "mongoose";
 import bookRoute from "./route/book.route.js";
+import userRoute from "./route/user.route.js"
+
+
 const app = express();
 app.use(cors());
-
+app.use(express.json());
 dotenv.config();
  
 const PORT = process.env.PORT || 4000;
@@ -28,6 +31,7 @@ try {
 //defining routes
 
 app.use("/book", bookRoute)
+app.use("/user", userRoute)
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`)
